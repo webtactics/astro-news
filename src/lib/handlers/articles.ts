f7bab7e93563a60e4536a9e8c540a7a970bf1ba2
Mohammad Rahmani <13013819+Mrahmani71@users.noreply.github.com>
2024-12-03 09:37:04 +0100

    optimise schema and articles

---
+++ src/lib/handlers/articles.ts
@@ -0,0 +1,15 @@
+import { getCollection } from "astro:content";
+
+const articlesCollection = await getCollection('articles');
+
+export const articlesHandler = {
+
+    allArticles: () => articlesCollection.filter((article) => article.data.isDraft !== true),
+
+    headlineBig: () => articlesCollection.filter((article) => article.data.isDraft !== true && article.data.isBigHeadline === true)[0],
+
+    headlineSmall: () => {
+        const bigHeadline = articlesHandler.headlineBig();
+        return articlesCollection.filter( async (article) => article.data.isDraft !== true && article.data.isSmallHeadline === true && bigHeadline.id !== article.id).slice(0, 4);
+    }
+}
--- src/lib/schema/index.ts
+++ src/lib/schema/index.ts
@@ -1,6 +1,9 @@
 import { z } from "astro:content";
 
 export const articleSchema = z.object({
+  isDraft: z.boolean().default(false),
+  isBigHeadline: z.boolean().default(false),
+  isSmallHeadline: z.boolean().default(false),
   title: z.string().max(60, "Too long, max 60 characters"),
   description: z.string().max(160, "Too long, max 160 characters"),
   category: z.string(),
